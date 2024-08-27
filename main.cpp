5670d01bf06bd0828549a9acbc5632aaae639c38
krypdkat <39078779+krypdkat@users.noreply.github.com>
2024-08-27 12:00:50 +0700

    move debug log to outside

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 #include "logger.h"
 #include <stdexcept>
 
-#undef ARBITRATOR
 #define ARBITRATOR "MEFKYFCDXDUILCAJKOIKWQAPENJDUHSSYPBRWFOTLALILAYWQFDSITJELLHG"
 #define DEBUG 0
 
@@ -19,6 +18,15 @@ T charToNumber(char *a) {
     char *endptr = nullptr;
     retVal = strtoull(a, &endptr, 10);
     return retVal;
+}
+static void printDebug(const char *fmt, ...)
+{
+#if DEBUG
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+#endif
 }
 
 void getTickData(QCPtr &qc, const uint32_t tick, TickData &result) {
